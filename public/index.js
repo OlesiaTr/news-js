df46151d878e0aa246ffa4d36e65407d24b4138f
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-30 21:20:59 +0200

    Revert "Async race"

--- public/index.js
+++
@@ -1,9 +0,0 @@
-import storeUpdate from './js/store/storeUpdate.js';
-import renderRoot from './js/ui-render/renderRoot.js';
-
-const initPage = async () => {
-  await storeUpdate();
-  await renderRoot();
-};
-
-initPage();
